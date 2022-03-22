76d6d065c3f55cce28853a3fa18a4074941a7987
gpawar2 <gpawar2@INLEN8520041975.corp.capgemini.com>
2022-03-22 16:27:19 +0530

    change 16:27

--- OnlineQuizManagementSystem/src/main/java/com/quiz/controller/QuestionController.java
+++ OnlineQuizManagementSystem/src/main/java/com/quiz/controller/QuestionController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.exception.QuestionNoNotFoundException;
-import com.quiz.model.Categories;
+
 import com.quiz.model.Question;
 import com.quiz.service.IQuestionService;
 
